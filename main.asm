a18d0fbde6e402e2c82552ba5f2edb1ca391074c
JulianWilkison-Duran <clockworksyler@gmail.com>
2025-01-17 23:14:52 -0500

    paddle hits the ball

--- main.asm
+++ main.asm
@@ -121,6 +121,28 @@ WaitVBlank2:
     cp 144
     jp c, WaitVBlank2
 
+    ; First, check if the ball is low enough to bounce off the paddle.
+    ld a, [_OAMRAM]
+    ld b, a
+    ld a, [_OAMRAM + 4]
+    cp a, b
+    jp nz, PaddleBounceDone ; If the ball isn't at the same Y position as the paddle, it can't bounce.
+    ; Now let's compare the X positions of the objects to see if they're touching.
+    ld a, [_OAMRAM + 5] ; Ball's X position.
+    ld b, a
+    ld a, [_OAMRAM + 1] ; Paddle's X position.
+    sub a, 8
+    cp a, b
+    jp nc, PaddleBounceDone
+    add a, 8 + 16 ; 8 to undo, 16 as the width.
+    cp a, b
+    jp c, PaddleBounceDone
+
+    ld a, -1
+    ld [wBallMomentumY], a
+
+PaddleBounceDone:
+
 ; Add the ball's momentum to its position in OAM.
     ld a, [wBallMomentumX]
     ld b, a
